64a8c008bb82f1cf05f79f158b2fbef990d76b83
GopiMunipalli <munipallegopikrishna@gmail.com>
2025-02-23 19:16:57 +0530

    vercel

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: [
-      'http://localhost:5175',
-      'http://localhost:5173',
-      'https://nestjs-umber.vercel.app',
-    ],
+    origin: ['http://localhost:5175', 'http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
